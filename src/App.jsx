4df63a13563b34ba027d5b699460233b287816e0
Kirnic21 <marcelokirnic@gmail.com>
2023-10-04 13:32:06 -0300

    Add routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-// App.jsx
+import { Link } from "react-router-dom";
 
-const App = () => <h1>Our First Test</h1>;
-
-export default App;
+  const App = () => {
+    return (
+      <div>
+        <h1>Hello from the main page of the app!</h1>
+        <p>Here are some examples of links to other pages</p>
+        <nav>
+          <ul>
+            <li>
+              <Link to="profile">Profile page</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  };
+  
+  export default App;
+  
